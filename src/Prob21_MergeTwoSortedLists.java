116bd8755d5efe06eeb625ab19186d86e3fe546b
Rabeet <raf6000@gmail.com>
2017-05-16 21:33:47 -0400

    Merge two sorted lists and reverse vowels of a string

---
+++ src/Prob21_MergeTwoSortedLists.java
@@ -0,0 +1,60 @@
+public class Prob21_MergeTwoSortedLists {
+	
+	public static void main(String[] args) {
+		ListNode node = new ListNode(2);
+//		node.next = new ListNode(4);
+		ListNode node1 = new ListNode(1);
+//		node1.next = new ListNode(52);
+//		node1.next.next = new ListNode(23);
+		ListNode ns = mergeTwoLists(node, node1);
+		while (ns != null) {
+			System.out.println(ns.val);
+			ns = ns.next;
+		}
+	}
+	
+	public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		if (l1 == null)
+			return l2;
+		if (l2 == null)
+			return l1;
+		ListNode node, copy;
+		if (l1.val <= l2.val) {
+			node = l1;
+			l1 = l1.next;
+		} else {
+			node = l2;
+			l2 = l2.next;
+		}
+		copy = node;
+		for (;;) {
+			if (l2 == null && l1 == null)
+				break;
+			else if(l2 != null && l1 == null) {
+				node.next = l2;
+				l2 = l2.next;
+			}
+			else if(l1 != null && l2 == null) {
+				node.next = l1;
+				l1 = l1.next;
+			}
+			else if (l1.val <= l2.val) {
+				node.next = l1;
+				l1 = l1.next;
+			}
+			else {
+				node.next = l2;
+				l2 = l2.next;
+			}
+			node = node.next;
+		}
+		return copy;
+	}
+	
+	static class ListNode {
+		ListNode next;
+		int val;
+		public ListNode (int val) { this.val = val;}
+	}
+
+}
--- src/Prob345_ReverseVowelsOfAString.java
+++ src/Prob345_ReverseVowelsOfAString.java
@@ -5,26 +5,33 @@ public class Prob345_ReverseVowelsOfAString {
 
 	public static void main(String[] args) {
 		Prob345_ReverseVowelsOfAString prog = new Prob345_ReverseVowelsOfAString();
-		System.out.println(prog.reverseVowels("my name is rabeet"));
-		System.out.println("print from seond:");
-		System.out.println(prog.reverseVowels("  1"));
+		System.out.println(prog.reverseVowels("YOYEAfu"));
 	}
 
 	public String reverseVowels(String s) {
-		if (s.length() < 1)
+		if (s == null || s.length() < 2)
 			return s;
-		String[] tokens = s.split(" ");
-		String ans = "";
-		for (int i = tokens.length - 1; i >= 0; i--) {
-			if (!tokens[i].isEmpty()) {
-				if (i == tokens.length - 1) {
-					ans += tokens[i];
-				} else {
-					ans += " " + tokens[i];
+		char[] c = s.toCharArray();
+		int j = c.length-1;
+		for (int i =0; i <j; i++) {
+			if (isVowel(c[i])) {
+				while (j > i) {
+					if (isVowel(c[j])) {
+						char temp = c[j];
+						c[j] = c[i];
+						c[i] = temp;
+						j--;
+						break;
+					}
+					j--;
 				}
 			}
 		}
-		return ans;
+		return String.valueOf(c);
+	}
+	
+	public boolean isVowel(char c) {
+		return (""+c).matches("[aAeEiIoOuU]");
 	}
 
 }

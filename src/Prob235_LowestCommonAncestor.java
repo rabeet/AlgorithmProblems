79f9ca7608f480742c15acd078c932faff503c1c
Rabeet <raf6000@gmail.com>
2017-09-22 05:44:25 -0400

    easy prob

---
+++ src/Prob235_LowestCommonAncestor.java
@@ -0,0 +1,12 @@
+
+public class Prob235_LowestCommonAncestor {
+	
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if ((p.val>= root.val && q.val <= root.val) || (p.val <= root.val && q.val >= root.val)) {
+            return root;
+        }
+        return (p.val <= root.val && q.val <= root.val) ? lowestCommonAncestor(root.left, p, q) : lowestCommonAncestor(root.right, p, q);
+    }
+
+}

a94119d12aa08c9a6e26ec43cf671a79e39061fc
Rabeet <raf6000@gmail.com>
2018-05-02 12:16:06 -0400

    Bottom up sol of coin change!

--- src/LeetCode/Prob322_CoinChange.java
+++ src/LeetCode/Prob322_CoinChange.java
@@ -9,6 +9,7 @@ public class Prob322_CoinChange {
         System.out.println(p.coinChangeRecursive(new int[]{1,2,5}, 11));
         System.out.println(p.coinChangePureRecursive(new int[]{1,2,5}, 11));
         System.out.println(p.coinChangeDynamic(new int[]{1,2,5}, 11));
+        System.out.println(p.coinChangeDynamicBottomUp(new int[]{1,2,5}, 11));
     }
 
     // remainder, number of tries to get to remainder will be the memoization table
@@ -25,7 +26,40 @@ public class Prob322_CoinChange {
     }
 
 
-    // TODO: Do the bottom up way using iterations
+    /*
+    1,2,5; 11
+    0,1,2,3,4,5,6,7,8,9,10,11
+      ^ ^ ^ ^ ^ ^ ^ ^ ^ ^  ^
+      1 1 2 2 1 2 2 3 3 2  3
+
+
+    for each int: i in rem
+        for each coin:
+            min coins needed to get to get to i using rem[i-coin]
+
+    if rem[amount] is 0 return -1 else return rem[amount]
+     */
+    private int coinChangeDynamicBottomUp(int[] coins, int amount) {
+        if (amount <0 || coins == null || coins.length == 0) return -1;
+        if (amount == 0) return 0;
+        int[] rem = new int[amount+1];
+        Arrays.fill(rem, amount+1);
+        rem[0] = 0;
+        for (int i = 1; i <rem.length; i++) {
+            for (int coin : coins) {
+                if (i == coin) {
+                    rem[i] = 1;
+                    break;
+                }
+                int temp = i - coin;
+                if (temp > 0) {
+                    rem[i] = Math.min(rem[i], rem[temp]+1);
+                }
+            }
+        }
+        System.out.println(Arrays.toString(rem));
+        return rem[amount] == amount+1 ? -1 : rem[amount];
+    }
 
     // Top Down - number of tries to get to the remainder
     private int coinChangeDynamic(int[] coins, int amount, int[] rem) {

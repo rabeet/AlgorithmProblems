96a005081bcb4e797c002848063414618d616900
Rabeet <raf6000@gmail.com>
2018-05-20 19:01:08 -0400

    Prob119 pascals triangle

--- src/LeetCode/Prob119_PascalsTriangleII.java
+++ src/LeetCode/Prob119_PascalsTriangleII.java
@@ -1,13 +1,16 @@
 package LeetCode;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Prob119_PascalsTriangleII {
 	
 	public static void main(String[] args) {
-		
+		System.out.println(getRow(3));
+		System.out.println(pascalsTriangle(3));
 	}
-	
+
+	// NOT WORKING, dont know when was this..?
 	public static List<Integer> pascalsTriangle(int n) {
 		List<Integer> list = new ArrayList<Integer>();
 		int[] triangle = new int[n];
@@ -17,6 +20,23 @@ public class Prob119_PascalsTriangleII {
 				triangle[j] += triangle[j]+triangle[j-1];
 			}
 		}
+		for(int i = 0; i < triangle.length; i++)
+			list.add(triangle[i]);
+		return list;
+	}
+
+	public static List<Integer> getRow(int rowIndex) {
+		int[] arr = new int[rowIndex+1];
+		int n = arr.length;
+		arr[0] = 1;
+		for (int i = 0; i < rowIndex; i++) {
+			for (int j = n-1; j>= 1; j--) {
+				arr[j] += arr[j-1];
+			}
+		}
+		List<Integer> list = new ArrayList<>();
+		for(int i = 0; i < n; i++)
+			list.add(arr[i]);
 		return list;
 	}
 }
